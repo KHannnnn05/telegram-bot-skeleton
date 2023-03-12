2021d5ee6451818bdcc8d07191f9ef9426f52d6e
Shamil Abdulaev <112097588+KHannnnn05@users.noreply.github.com>
2023-03-12 12:17:27 +0300

    Update config.py

--- src/telegram/config.py
+++ src/telegram/config.py
@@ -5,4 +5,4 @@ from os.path import join, dirname
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-test_token = os.environ.get('TEST_BOT_TOKEN')
+token = os.environ.get('API_TOKEN')
